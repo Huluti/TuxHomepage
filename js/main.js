7fd135442e3069cc860b12386ce32edfd9a26c68
Huluti <hugo.posnic@protonmail.com>
2016-07-16 11:53:26 +0200

    Improve way to get JSON files

--- js/main.js
+++ js/main.js
@@ -2,46 +2,46 @@ $(document).ready(function () {
     var defaultDistrib = "ubuntu";
     var defaultSearchEngine = "qwant";
 
-    /* Return JSON Object */
-    function getJson(file) {
-        return JSON.parse($.ajax({
-            type: "GET",
-            url: file,
-            dataType: "json",
-            global: false,
-            async: false,
-            success: function (data) {
-                return data;
+    /* Initialize the web page with JSON files */
+    function initialize(name) {
+        file = "data/" + name + ".json";
+        $.getJSON(file, function (json) {
+            switch(name) {
+                case "distribs":
+                    changeDistrib(distrib, json);
+                    break;
+                case "engines":
+                    changeSearchEngine(search, json);
             }
-        }).responseText);
+        });
     }
 
     /* Change colors, logo & links */
-    function changeDistrib(name) {
+    function changeDistrib(name, json) {
         distrib = !name ? defaultDistrib : name;
 
         if (distrib != $("#logo-img").attr("class")) {
             $("#logo-img").attr("src", "img/" + distrib + "-logo.png").removeClass().addClass(distrib);
 
-            $("#search").css("background", "#" + distribs[distrib].main_color);
-            $("#navbar ul li a").css("color", "#" + distribs[distrib].main_color);
+            $("#search").css("background", "#" + json[distrib].main_color);
+            $("#navbar ul li a").css("color", "#" + json[distrib].main_color);
 
-            $(".site").attr("href", distribs[distrib].site).text("Site officiel");
-            $(".doc").attr("href", distribs[distrib].doc).text("Documentation");
+            $(".site").attr("href", json[distrib].site).text("Site officiel");
+            $(".doc").attr("href", json[distrib].doc).text("Documentation");
 
             Cookies.set("distrib", distrib, {expires: 365, path: "/"});
         }
     }
 
     /* Change search engine logo */
-    function changeSearchEngine(name) {
+    function changeSearchEngine(name, json) {
         search = !name ? defaultSearchEngine : name;
 
         if (search != $("#engine").attr("class")) {
-            $("form").attr("action", searchEngines[search].url);
-            $("input[type='search']").attr("name", searchEngines[search].parameter);
+            $("form").attr("action", json[search].url);
+            $("input[type='search']").attr("name", json[search].parameter);
             $("#engine").removeClass().addClass(search);
-            $("#engine").css({"background": "#" + searchEngines[search].colors.circle, "color": "#" + searchEngines[search].colors.font});
+            $("#engine").css({"background": "#" + json[search].colors.circle, "color": "#" + json[search].colors.font});
             $("#engine").html(search.charAt(0).toUpperCase());
 
             Cookies.set("engine", search, {expires: 365, path: "/"});
@@ -52,12 +52,9 @@ $(document).ready(function () {
     var distrib = Cookies.get("distrib");
     var search = Cookies.get("engine");
 
-    var distribs = getJson("data/distribs.json");
-    var searchEngines = getJson("data/engines.json");
-
-    /* Initialize the page */
-    changeDistrib(distrib);
-    changeSearchEngine(search);
+    /* Initialize the web page */
+    initialize("distribs");
+    initialize("engines");
 
     /* Select current options in selects */
     $("select[name='distribs']").val(distrib);

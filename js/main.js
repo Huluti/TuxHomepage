311d80f29a29faf95198772e5d57788cc9ab3016
Huluti <hugo.posnic@gmail.com>
2016-11-02 20:02:15 +0100

    Add comments to simplify code reading

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ var defaultDistrib = "ubuntu";
 var defaultEngine = "qwant";
 var defaultLanguage = "en";
 
-/* Cookies */
+/* Use cookies if defined, else use default values */
 var distrib = Cookies.get("distrib") ? Cookies.get("distrib") : defaultDistrib;
 var engine = Cookies.get("engine") ? Cookies.get("engine") : defaultEngine;
 var language = Cookies.get("language") ? Cookies.get("language") : defaultLanguage;
 
-/* Read JSON file */
+/* Read JSON file, then call callback function */
 function loadJSON(filename, callback) {
     var request = new XMLHttpRequest();
     request.open("GET", "data/" + filename, true);
@@ -41,18 +41,24 @@ function createLinks(json) {
 /* Change distrib elements */
 function changeDistrib(json) {
     var logo = document.getElementById("logo-img");
+    /* Change if new distrib is not current distrib */
     if (distrib !== logo.className) {
+        /* Change logo of distrib */
         logo.setAttribute("src", "img/" + json[distrib].name.toLowerCase() + "-logo.png");
         logo.className = distrib;
+        /* Change color of search div */
         document.getElementById("search").style.backgroundColor = "#" + json[distrib].main_color;
+        /* Change colors of links in navbar */
         var links = document.querySelectorAll("#navbar a");
         for (var i = 0; i < links.length; i++) {
             links[i].style.color = "#" + json[distrib].main_color;
         }
+        /* Change specific distrib links */
         var website = document.querySelector(".website");
         website.setAttribute("href", json[distrib].website);
         var doc = document.querySelector(".doc");
         doc.setAttribute("href", json[distrib].doc);
+        /* Set distrib cookie with new value */
         Cookies.set("distrib", distrib, {expires: 365, path: "/"});
     }
 }
@@ -60,23 +66,29 @@ function changeDistrib(json) {
 /* Change search elements */
 function changeEngine(json) {
     var engineCircle = document.getElementById("engine");
+    /* Change if new engine is not current engine */
     if (engine !== engineCircle.className) {
+        /* Set parameters to do search */
         document.getElementById("form-search").setAttribute("action", json[engine].url);
         document.querySelector("input[type='search']").setAttribute("name", json[engine].parameter);
         engineCircle.className = engine;
+        /* Change style of engine circle */
         engineCircle.style.backgroundColor = "#" + json[engine].colors.circle;
         engineCircle.style.color = "#" + json[engine].colors.font;
         engineCircle.textContent = engine.charAt(0).toUpperCase();
+        /* Set engine cookie with new value */
         Cookies.set("engine", engine, {expires: 365, path: "/"});
     }
 }
 
-/* Change lang of all texts */
+/* Translate webpage */
 function changeLanguage(json) {
+    /* Change lang of languages select */
     var options = document.querySelectorAll("#languages option");
     for (var i = 0; i < options.length; i++) {
         options[i].textContent = json[language][options[i].value + "_msg"];
     }
+    /* Change lang of all texts */
     document.querySelector("#input-search input").placeholder = json[language].placeholder_msg;
     document.querySelector(".website").textContent = json[language].website_msg;
     document.querySelector(".doc").textContent = json[language].doc_msg;
@@ -91,7 +103,7 @@ function changeLanguage(json) {
     Cookies.set("language", language, {expires: 365, path: "/"});
 }
 
-/* Fill selects */
+/* Fill select */
 function fillSelect(data) {
     var select = document.querySelector("#" + data[0]);
     data[1].forEach(function (elt) {
